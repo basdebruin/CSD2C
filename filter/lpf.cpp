8bc992e7657c077c18b4cfb032ae6160f26c98ff
Bas de Bruin <bruin998@gmail.com>
2020-03-03 12:17:31 +0100

    improved comments

--- filter/lpf.cpp
+++ filter/lpf.cpp
@@ -5,14 +5,14 @@
 LPF::LPF(float cutoff, float resonance, float samplerate) {
     this->cutoff = cutoff;
     this->resonance = resonance;
-    // convert cutoff and res
+    // convert cutoff and res to formula values
     c = pow(0.5, (128 - (cutoff / 24000 * 128)) / 16.0);
     r = pow(0.5, ((resonance * 128) + 24) / 16.0);
 };
 
 LPF::~LPF() {};
 
-// alternative algoritm with resonance, a lot of it
+// algoritm with resonance, a lot of it
 float LPF::update(float input) {
 
     // v0 = (1 - r * c) * v0 - (c) * v1 + (c) * input;
@@ -21,5 +21,5 @@ float LPF::update(float input) {
 
     return output = tanh(v1); //added tanh() saturation as a form of limiting, the resonance can be VERY loud
 
-    // source: https://www.musicdsp.org/en/latest/Filters/185-1-rc-and-c-filter.html
+    // based on: https://www.musicdsp.org/en/latest/Filters/185-1-rc-and-c-filter.html
 }
